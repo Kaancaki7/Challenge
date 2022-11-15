d5b1fe1679975091fb79327ec3ecb14ec8da33f1
kaan <kaancaki0@gmail.com>
2022-11-15 18:15:42 +0300

    hackerrank

---
+++ src/javaifelse/JavaIfElse.java
@@ -0,0 +1,22 @@
+package javaifelse;
+import java.util.Scanner;3
+public class JavaIfElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 2 != 0) {
+            System.out.println("Weird");
+        }
+        if (n % 2 == 0) {
+            if (n < 5 && n > 2)
+                System.out.println("Not Weird");
+            else if (n > 6 && n < 20)
+                System.out.println("Weird");
+            else if (n > 20)
+                System.out.println("Not Weird");
+            else
+                System.out.println("Weird");
+
+        }
+    }
+}
